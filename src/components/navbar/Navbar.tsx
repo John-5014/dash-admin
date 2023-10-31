8b5c6bd02167c8c3bd13fba5f50841393b79ae40
John-5014 <kaiserjohn52@gmail.com>
2023-10-31 04:23:21 +0300

    add navbar icons and styling

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,28 @@
 import "./Navbar";
+import "./navbar.scss"
 
 const Navbar = () => {
     return(
-        <div className="navbar">Navbar</div>
+        <div className="navbar">
+            <div className="logo">
+                <img src="/public/logo.svg" alt="" />
+                <span>Kaiser-admin</span>
+            </div>
+            <div className="icons">
+                <img src="/public/search.svg" alt="" className="icon" />
+                <img src="/public/app.svg" alt="" className="icon" />
+                <img src="/public/expand.svg" alt="" className="icon" />
+                <div className="notification">
+                    <img src="/public/notifications.svg" alt="" />
+                    <span>1</span>
+                </div>
+                <div className="user">
+                    <img src="/public/pexels-sheikh-basim-18419151 (1).jpg" alt="" />
+                    <span>John</span>
+                </div>
+                <img src="/public/setting.svg" alt="" className="icon" />
+            </div>
+        </div>
     )
 }
 
