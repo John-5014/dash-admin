5f1af44263a936b931e08230b612e11813ea0184
John-5014 <kaiserjohn52@gmail.com>
2023-10-31 04:34:11 +0300

    footer content

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,11 @@
 import "./Footer";
-
+import "./footer.scss";
 const Footer = () => {
     return(
-        <div className="footer">footer</div>
+        <div className="footer">
+            <span>Kaiser-admin</span>
+            <span> Admin Dashboard</span>
+        </div>
     )
 }
 
