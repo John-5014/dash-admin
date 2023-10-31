1efab9118d9e3ede90c57982f8b77de060b34d57
John-5014 <kaiserjohn52@gmail.com>
2023-10-31 03:30:16 +0300

    add login pages

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,20 @@
+// import "./login.scss";
+
+// const Login = () => {
+//     return (
+//         <div className = "login">Hello login</div>
+//     )
+// }
+
+// export default Login;
+
+
+const Login = () => {
+  return (
+    <div>
+      Login
+    </div>
+  )
+}
+
+export default Login;
