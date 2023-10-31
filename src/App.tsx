ce3e1ca5ebc6b300750efd0d1836ec085838f2fb
John-5014 <kaiserjohn52@gmail.com>
2023-10-31 03:28:12 +0300

    add layout and pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,62 @@ import { createRoot } from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
-  Link,
+  Outlet
 } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Products from "./pages/products/Products";
+import Login from "./pages/Login/Login";
 import Users from "./pages/users/Users";
+import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/menu/Menu";
+import Footer from "./components/footer/Footer";
     
 
 function App() {
 
 const Layout = () => {
   return(
-    <div className="main"></div>
+    <div className="main">
+      <Navbar/>
+      <div className="container">
+      <div className="menucontainer">
+        <Menu/>
+      </div>
+      <div className="container">
+        <Outlet/>
+
+      </div>
+      </div>
+      <Footer/>
+    </div>
   )
 }
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: (
-      <Home/>
-    ),
+    element: <Layout />,
+    children:[
+      {
+      path: "/",
+      element:<Home/>
+    },
+    {
+      path: "/users",
+      element:<Users/>
+    },
+    {
+      path: "/products",
+      element:<Products/>
+    },
+    
+  ]
+  
   },
   {
-    path: "users",
-    element: (
-      <Users/>
-    ),
-  },
-  {
-    path: "products",
-    element: <Products/>,
-  },
+    path: "/login",
+    element:<Login/>
+  }
 ]);
   return (
     <RouterProvider router = {router} />
